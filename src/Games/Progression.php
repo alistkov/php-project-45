36b85577b43d6898792d992d6643ef0ee62178eb
Anton Listkov <antonlistkov@gmail.com>
2023-06-06 10:14:05 +0300

    finish progression game

---
+++ src/Games/Progression.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BrainGames\Even;
+
+use function BrainGames\Engine\startGame;
+
+function createProgression(): array
+{
+    $progression = [];
+    $count = rand(5, 10);
+    $missingIndex = rand(0, $count);
+    $startValue = rand(2, 10);
+    $step = rand(2, 10);
+    $missingElement = null;
+
+    for ($i = 0; $i <= $count; $i += 1) {
+        if ($i === $missingIndex) {
+            $progression[] = '..';
+            $missingElement = $startValue + $step * $i;
+        } else {
+            $progression[] = $startValue + $step * $i;
+        }
+    }
+    return [$progression, $missingElement];
+}
+
+function startBrainProgressionGame(): void
+{
+    $rules = 'What number is missing in the progression?';
+    $rounds = 3;
+    $gameData = [];
+    for ($i = 0; $i < $rounds; $i += 1) {
+        [$progression, $missingElement] = createProgression();
+        $question = implode(', ', $progression);
+        $correctAnswer = (string) $missingElement;
+        $gameData[] = [$question, $correctAnswer];
+    }
+    startGame($rules, $gameData);
+}
