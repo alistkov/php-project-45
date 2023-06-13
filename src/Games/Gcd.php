e42d83bc2ed022179c83a5120c7281008c372564
Anton Listkov <antonlistkov@gmail.com>
2023-06-13 23:09:57 +0300

    replace gcd function

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -4,6 +4,14 @@ namespace BrainGames\Gcd;
 
 use function BrainGames\Engine\startGame;
 
+function getGcd(int $firstNumber, int $secondNumber): int
+{
+    if ($secondNumber === 0) {
+        return $firstNumber;
+    }
+    return getGcd($secondNumber, $firstNumber % $secondNumber);
+}
+
 function startBrainGCDGame(): void
 {
     $rules = 'Find the greatest common divisor of given numbers.';
@@ -12,7 +20,7 @@ function startBrainGCDGame(): void
     for ($i = 0; $i < $rounds; $i += 1) {
         $firstRandomNumber = rand(1, 50);
         $secondRandomNumber = rand(1, 50);
-        $correctAnswer = gmp_gcd($firstRandomNumber, $secondRandomNumber);
+        $correctAnswer = getGcd($firstRandomNumber, $secondRandomNumber);
         $question = "{$firstRandomNumber} {$secondRandomNumber}";
         $gameData[] = [$question, (string) $correctAnswer];
     }
